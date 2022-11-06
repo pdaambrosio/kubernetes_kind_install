223fe1bef8f614f06159a3aec97533a741868339
pdaambrosio <pda.ambrosio@gmail.com>
2022-11-06 20:32:19 -0300

    feat: instalation

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # kubernetes_kind_install
+
 Kind is a tool for running local Kubernetes clusters using Docker container “nodes”.
+Kind was primarily designed for testing Kubernetes itself, but may be used for local development or CI.
+
+## Installation
+
+### Linux
+
+```bash
+chmod +x kind_install.sh
+./kind_install.sh
+```
---
+++ install_kind copy.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+set -o errexit
+set -o nounset
+set -o pipefail
+
+# Install kind
+curl -Lo ./kind https://kind.sigs.k8s.io/dl/v0.17.0/kind-linux-amd64
+chmod +x ./kind
+mv ./kind /usr/local/bin/
+echo "source <(kind completion bash)" >> ~/.zshrc
+echo "kind installed"
+
+# Install kubectl
+curl -Lo https://storage.googleapis.com/kubernetes-release/release/v1.17.0/bin/linux/amd64/kubectl
+chmod +x ./kubectl
+mv ./kubectl /usr/local/bin/
+echo "kubectl installed"
+
+# Create kind cluster
+kind create cluster --config kind-config.yaml
+echo "kind cluster created"
+
+# Install helm
+curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3
+chmod 700 get_helm.sh
+./get_helm.sh
+echo "helm installed"
+
+# Install kubectx
+git clone https://github.com/ahmetb/kubectx /opt/kubectx
+ln -s /opt/kubectx/kubectx /usr/local/bin/kubectx
+ln -s /opt/kubectx/kubens /usr/local/bin/kubens
+mkdir -p ~/.oh-my-zsh/completions
+chmod -R 755 /opt/kubectx/completion
+ln -s /opt/kubectx/completion/kubectx.zsh ~/.oh-my-zsh/completions/_kubectx.zsh
+ln -s /opt/kubectx/completion/kubens.zsh ~/.oh-my-zsh/completions/_kubens.zsh
+echo "kubectx installed"
+
+# Install k9s
+curl -sS https://webinstall.dev/k9s | bash
+echo "k9s installed"
+
+# Install kubetail
+curl -sS https://webinstall.dev/kubetail | bash
+echo "kubetail installed"
